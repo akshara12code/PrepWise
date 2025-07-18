49c714552ff1cc3eb09856b9c97f95241bb74ae1
akshara12code <akshara12082005@gmail.com>
2025-07-18 20:56:09 +0530

    Your commit message here

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds:true,
-};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
